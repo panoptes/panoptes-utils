c8b648fccca3ff5e8cdf8c624517119d8b7b6d8d
Wilfred Gee <wtylergee@gmail.com>
2019-04-02 15:23:39 +1100

    Convenienve options for installing all options

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 # Licensed under an MIT style license - see LICENSE.txt
 
 try:
-    from setuptools import setup, find_packages
+  from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+  from distutils.core import setup
+
+import itertools
 
 from configparser import ConfigParser
 from distutils.command.build_py import build_py
@@ -53,6 +55,7 @@ setup(name=PACKAGENAME,
           'mongo': modules['mongo'],
           'social': modules['social'],
           'testing': modules['testing'],
+          'all': list(set(itertools.chain.from_iterable(modules.values())))
       },
       packages=find_packages(exclude=['tests', 'test_*']),
       classifiers=[
