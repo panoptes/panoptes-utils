9553c6ba600b11d3772246505442faa689cc4057
Wilfred Gee <wtylergee@gmail.com>
2019-04-02 20:42:30 +1100

    Add convenience script

---
+++ scripts/start_docker_config_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+usage() {
+  echo -n "##################################################
+# Start the config param server.
+# 
+# This will start a docker container running the config server.
+##################################################
+ $ $(basename $0)
+ 
+ Example:
+  ./start_docker_config_server.sh
+"
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+FNAME=$1
+TITLE="${2}"
+
+docker run --rm \
+	--name config_server \
+	-p 6563:6563 \
+	-v /var/panoptes:/var/panoptes \
+	gcr.io/panoptes-survey/panoptes-utils \
+	python3 scripts/run_config_server.py --public
