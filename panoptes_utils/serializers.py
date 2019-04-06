7df3c5930d564f04cb854907c076b4f97241b4c0
Wilfred Gee <wtylergee@gmail.com>
2019-04-06 19:54:11 +1100

    Removing the json file convenience functions for now as they are unused and lagging behind.

--- panoptes_utils/serializers.py
+++ panoptes_utils/serializers.py
@@ -53,46 +53,6 @@ def _parse_quantities(obj):
         return obj
 
 
-def dumps_file(fn, obj, clobber=False):
-    """Convenience warpper to dump an object to a a file.
-
-    Args:
-        fn (str): Path of filename where object representation will be saved.
-        obj (dict): An object to serialize.
-        clobber (bool, optional): If object should be overwritten or appended to.
-            Defaults to False, which will append to file.
-
-    Returns:
-        str: Filename of the file that was written to.
-    """
-    if clobber is True:
-        mode = 'w'
-    else:
-        mode = 'a'
-
-    with open(fn, mode) as f:
-        f.write(orjson.dumps(obj, default=_serializer) + "\n")
-
-    return fn
-
-
-def loads_file(file_path):
-    """Convenience wrapper to load an object from a file.
-
-    Args:
-        file_path (str): Path of filename that contains a serialization of the
-            the object.
-
-    Returns:
-        dict: The loaded object from the given file.
-    """
-    obj = None
-    with open(file_path, 'r') as f:
-        obj = orjson.loads(f.read())
-
-    return obj
-
-
 def _serializer(obj):
     if isinstance(obj, u.Quantity):
         return {'value': obj.value, 'unit': obj.unit.name}
