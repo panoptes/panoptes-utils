7d1c5d0ce3d19629c0dad8bbd3b489c41e14f5bc
Wilfred Gee <wtylergee@gmail.com>
2019-04-06 19:53:41 +1100

    * Making simply names: `from_json` and `to_json` * Adding doctests * Proper handling of astropy quantities

--- panoptes_utils/serializers.py
+++ panoptes_utils/serializers.py
@@ -1,28 +1,56 @@
-from bson import json_util
+import orjson
+
+from astropy import units as u
 
 
-def dumps(obj):
-    """Dump an object to JSON.
+def to_json(obj):
+    """Convert a Python object to a JSON string.
+
+    >>> from panoptes_utils.serializers import to_json
+    >>> from astropy import units as u
+    >>> config = { "location": { "name": "Mauna Loa", "elevation": 3397 * u.meter } }
+    >>> to_json(config)
+    '{"location":{"name":"Mauna Loa","elevation":{"value":3397.0,"unit":"m"}}}'
 
     Args:
-        obj (dict): An object to serialize.
+        obj (any): The object to be converted to JSON, usually a dict.
 
     Returns:
-        str: Serialized representation of object.
+        str: The JSON string representation of the object.
     """
-    return json_util.dumps(obj)
+    return orjson.dumps(obj, default=_serializer).decode('utf8')
 
 
-def loads(msg):
-    """Load an object from JSON.
+def from_json(msg):
+    """Convert a JSON string into a Python object.
+
+    >>> from panoptes_utils.serializers import from_json
+    >>> config_str = '{"location":{"name":"Mauna Loa","elevation":{"value":3397.0,"unit":"m"}}}'
+    >>> from_json(config_str)
+    {'location': {'name': 'Mauna Loa', 'elevation': <Quantity 3397. m>}}
 
     Args:
-        msg (str): A serialized string representation of object.
+        msg (str): The JSON string representation of the object.
 
     Returns:
         dict: The loaded object.
     """
-    return json_util.loads(msg)
+    return _parse_quantities(orjson.loads(msg))
+
+
+def _parse_quantities(obj):
+    # If there are exactly two keys
+    keys = sorted(list(obj.keys()))
+    if keys == ['unit', 'value']:
+        return obj['value'] * u.Unit(obj['unit'])
+    else:
+        for k in keys:
+            if isinstance(obj[k], dict):
+                new_obj = _parse_quantities(obj[k])
+                if new_obj is not None:
+                    obj[k] = new_obj
+
+        return obj
 
 
 def dumps_file(fn, obj, clobber=False):
@@ -43,7 +71,7 @@ def dumps_file(fn, obj, clobber=False):
         mode = 'a'
 
     with open(fn, mode) as f:
-        f.write(dumps(obj) + "\n")
+        f.write(orjson.dumps(obj, default=_serializer) + "\n")
 
     return fn
 
@@ -60,6 +88,11 @@ def loads_file(file_path):
     """
     obj = None
     with open(file_path, 'r') as f:
-        obj = loads(f.read())
+        obj = orjson.loads(f.read())
 
     return obj
+
+
+def _serializer(obj):
+    if isinstance(obj, u.Quantity):
+        return {'value': obj.value, 'unit': obj.unit.name}
