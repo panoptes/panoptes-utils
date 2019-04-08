7d5780fd4d5fcc8d766b090af12d030b1797a3a3
Wilfred Gee <wtylergee@gmail.com>
2019-04-08 18:34:15 +1000

    Updates for better looking docs

--- panoptes_utils/serializers.py
+++ panoptes_utils/serializers.py
@@ -20,8 +20,8 @@ def to_json(obj):
     '{"numpy_array":[0,1,2,3,4,5,6,7,8,9]}'
 
     >>> from panoptes_utils import current_time
-    >>> to_json({"current_time": current_time()})
-    '{"current_time":...-...-...T...:...:..."}'
+    >>> to_json({"current_time": current_time()})       # doctest: +SKIP
+    '{"current_time":"2019-03-27T16:42:01.001"}'
 
     Args:
         obj (any): The object to be converted to JSON, usually a dict.
@@ -35,32 +35,34 @@ def to_json(obj):
 def from_json(msg):
     """Convert a JSON string into a Python object.
 
-    Astropy quanitites will be converted from a `{"value": val, "unit": unit}` format.
+    Astropy quanitites will be converted from a ``{"value": val, "unit": unit}`` format.
     Time-like values are *not* parsed, however see example below.
 
-    >>> from panoptes_utils.serializers import from_json
-    >>> config_str = '{"location":{"name":"Mauna Loa","elevation":{"value":3397.0,"unit":"m"}}}'
-    >>> from_json(config_str)
-    {'location': {'name': 'Mauna Loa', 'elevation': <Quantity 3397. m>}}
+    .. doctest::
 
-    # Invalid values will be returned as is.
-    >>> from_json('{"horizon":{"value":42.0,"unit":"degr"}}')
-    {'horizon': {'value': 42.0, 'unit': 'degr'}}
+        >>> from panoptes_utils.serializers import from_json
+        >>> config_str = '{"location":{"name":"Mauna Loa","elevation":{"value":3397.0,"unit":"m"}}}'
+        >>> from_json(config_str)
+        {'location': {'name': 'Mauna Loa', 'elevation': <Quantity 3397. m>}}
 
-    # Be careful with short unit names!
-    >>> horizon = from_json('{"horizon":{"value":42.0,"unit":"d"}}')
-    >>> horizon['horizon']
-    <Quantity 42. d>
-    >>> horizon['horizon'].decompose()
-    <Quantity 3628800. s>
+        # Invalid values will be returned as is.
+        >>> from_json('{"horizon":{"value":42.0,"unit":"degr"}}')
+        {'horizon': {'value': 42.0, 'unit': 'degr'}}
 
-    >>> from panoptes_utils import current_time
-    >>> time_str = to_json({"current_time": current_time().datetime})
-    >>> from_json(time_str)['current_time']
-    '...-...-...T...:...:...'
-    >>> from astropy.time import Time
-    >>> Time(from_json(time_str)['current_time'])
-    <Time object: scale='utc' format='isot' value=...>
+        # Be careful with short unit names!
+        >>> horizon = from_json('{"horizon":{"value":42.0,"unit":"d"}}')
+        >>> horizon['horizon']
+        <Quantity 42. d>
+        >>> horizon['horizon'].decompose()
+        <Quantity 3628800. s>
+
+        >>> from panoptes_utils import current_time
+        >>> time_str = to_json({"current_time": current_time().datetime})
+        >>> from_json(time_str)['current_time']         # doctest: +SKIP
+        2019-04-08T06:43:28.232406
+        >>> from astropy.time import Time
+        >>> Time(from_json(time_str)['current_time'])   # doctest: +SKIP
+        <Time object: scale='utc' format='isot' value=2019-04-08T06:43:28.232>
 
     Args:
         msg (str): The JSON string representation of the object.
