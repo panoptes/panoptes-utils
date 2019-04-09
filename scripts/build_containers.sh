2ce87d3ca5212e0a8c4627bc0720d4d4450f27a9
Wilfred Gee <wtylergee@gmail.com>
2019-04-10 05:49:02 +1000

    Cleanup to get build working
    * Use `latest` tag instead of COMMIT_SHA.
    * Relax some requirements so we can pull more from conda.
    * 5 hour timeout! The arm builds take a long time, but mostly compiling
    things like matplotlib and scipy. The above from conda should help with
    that too.

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-COMMIT_SHA=$(git rev-parse HEAD) gcloud builds submit --timeout="2h" --config cloudbuild.yaml --async .
+COMMIT_SHA=$(git rev-parse HEAD) gcloud builds submit --timeout="5h" --config cloudbuild.yaml --async .
 
