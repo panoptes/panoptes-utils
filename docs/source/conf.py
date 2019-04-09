662d60b10c858fe0f0abf670c457ca3d518fe551
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-04-10 07:35:20 +1000

    Revert to simple theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,12 +86,8 @@ pygments_style = 'lovelace'
 # a list of builtin themes.
 #
 
-html_theme = 'guillotina'
+html_theme = 'sphinx_rtd_theme'
 
-
-def setup(app):
-    import sphinx_guillotina_theme
-    sphinx_guillotina_theme.setup(app)
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -7,7 +7,7 @@ PANOPTES Utilities
 ==================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
    :caption: Contents:
 
    panoptes_utils
