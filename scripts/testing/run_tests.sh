dfd956212aaf35d5b6a02983cd863ddd7f17f4a3
Wilfred Gee <wtylergee@gmail.com>
2019-04-11 19:03:46 +1000

    Try to explicitly force environment

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e 
 
+source /opt/conda/etc/profile.d/conda.sh
 conda activate panoptes-env
 export PYTHONPATH="$PYTHONPATH:$PANDIR/panoptes-utils/scripts/testing/coverage"
 export COVERAGE_PROCESS_START=${PANDIR}/panoptes-utils/.coveragerc
