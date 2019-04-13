277308d5d533d609bf3270593d33e932c0baae98
Wilfred Gee <wtylergee@gmail.com>
2019-04-13 20:35:40 +1000

    Still fixing up this run file. :)

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e 
 
-conda activate panoptes-env
 export PYTHONPATH="$PYTHONPATH:$PANDIR/panoptes-utils/scripts/testing/coverage"
 export COVERAGE_PROCESS_START=${PANDIR}/panoptes-utils/.coveragerc
 coverage run $(which pytest) -v --test-databases all
