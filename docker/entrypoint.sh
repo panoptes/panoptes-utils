9f455368880992616fa5c6623ba88c40bae48b5c
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-05-20 10:54:30 +1000

    Fix gcloud auth
    * Use service key if running from a unit but don't start sql proxy.
    * Start Cloud SQL proxy on GCE instance.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,18 @@
 
 echo "Setting up base environment"
 
-# Authenticate if key has been set
+# Authenticate if key has been set - used on local units
 if [ ! -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
     echo "Found Google credentials, activating service account"
     /root/google-cloud-sdk/bin/gcloud auth activate-service-account \
     --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+fi
 
+# Authenticate if on GCE
+if [ ! -z "$CLOUDSDK_COMPUTE_ZONE" ]; then
     echo "Starting Cloud SQL proxy"
     python ${PANDIR}/panoptes-utils/scripts/connect_cloud_sql_proxy.py \
         --config="${PANDIR}/panoptes-utils/docker/cloud-sql-conf.yaml" \
-        --key-file="$GOOGLE_APPLICATION_CREDENTIALS" \
         --verbose &
 
     echo "Getting DB passwords from metadata server"
