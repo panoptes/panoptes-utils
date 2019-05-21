30fbe5ee61f35eb02b1fd52c6aa482be82e7d1e3
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-05-22 06:43:38 +1000

    * Adding sextractor. * Docker updates     * Split into two separate images during build in attempt to minimize     rebuilding. Not clear this helped and I think did the opposite. And     increased size. So should probably remove.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,15 +14,19 @@ fi
 # Authenticate if on GCE
 if [ ! -z ${GOOGLE_COMPUTE_INSTANCE} ]; then
     echo "Getting Cloud SQL config from metadata server"
-    curl --silent "http://metadata.google.internal/computeMetadata/v1/project/attributes/cloud_sql_conf" -H "Metadata-Flavor: Google" > ${PANDIR}/conf_files/cloud-sql-conf.yaml
+    curl --silent "http://metadata.google.internal/computeMetadata/v1/project/attributes/cloud_sql_conf" -H "Metadata-Flavor: Google"
+    curl --silent "http://metadata.google.internal/computeMetadata/v1/project/attributes/cloud_sql_conf" -H "Metadata-Flavor: Google" > ${HOME}/.cloud-sql-conf.yaml
 
     echo "Getting DB passwords from metadata server"
     curl --silent "http://metadata.google.internal/computeMetadata/v1/project/attributes/pgpass" -H "Metadata-Flavor: Google" > ${HOME}/.pgpass
     chmod 600 ${HOME}/.pgpass
 
+    echo "Cloud SQL conf:"
+    cat ${HOME}/.cloud-sql-conf.yaml
+
     echo "Starting Cloud SQL proxy"
     python ${PANDIR}/panoptes-utils/scripts/connect_cloud_sql_proxy.py \
-        --config="${PANDIR}/conf_files/cloud-sql-conf.yaml" \
+        --config="${HOME}/.cloud-sql-conf.yaml" \
         --verbose &
 fi
 
