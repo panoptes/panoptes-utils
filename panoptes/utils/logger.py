53a9e40c947ecd0cfb9af3356d88134b747021da
Wilfred Gee <wtylergee@gmail.com>
2019-06-23 16:47:37 +1000

    Only silency urllib3, not requests

--- panoptes/utils/logger.py
+++ panoptes/utils/logger.py
@@ -16,7 +16,7 @@ from panoptes.utils.serializers import from_yaml
 from panoptes.utils.serializers import to_json
 
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
-logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
 
 
 # We don't want to create multiple root loggers that are "identical",
