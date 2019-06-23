dbf1afb8e0cff867c2d5d74d1d3948dfcd450eb3
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-06-24 09:06:03 +1000

    Try to get more verbose output on travis. Damn you travis.

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -e 
+#!/bin/bash -e
 
 export PYTHONPATH="$PYTHONPATH:$PANDIR/panoptes-utils/scripts/testing/coverage"
 export COVERAGE_PROCESS_START=${PANDIR}/panoptes-utils/.coveragerc
-coverage run $(which pytest) -v --test-databases all
+coverage run $(which pytest) -vvrs --test-databases all
 
 # Upload coverage reports if running from Travis.
 if [[ $TRAVIS ]]; then
