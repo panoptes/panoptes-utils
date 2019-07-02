ac45ae6b35ae8e656746e2c0a9914172024fe94b
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-07-02 15:25:47 +1000

    Match POCS scripts

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -e
 
+cd "${PANDIR}/panoptes-utils"
+
 # Install any updated requirements
 pip install -r requirements.txt
 
 export PYTHONPATH="$PYTHONPATH:$PANDIR/panoptes-utils/scripts/testing/coverage"
-export COVERAGE_PROCESS_START=${PANDIR}/panoptes-utils/.coveragerc
-coverage run $(which pytest) -vvrs --test-databases all
+export COVERAGE_PROCESS_START=.coveragerc
+coverage run $(command -v pytest) -vvrs --test-databases all
 
 # Upload coverage reports if running from Travis.
 if [[ $TRAVIS ]]; then
 	coverage combine
 	bash <(curl -s https://codecov.io/bash)
 fi
+
+exit 0
