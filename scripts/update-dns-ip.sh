284d453b7249869519efc4c45f8b3e89325df9ea
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-07-03 21:26:44 +1000

    Update to update dns script

--- scripts/update-dns-ip.sh
+++ scripts/update-dns-ip.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -e
 
+# Put this in a cronjob so name is updated on each reboot, i.e. crontab -e
+# @reboot /home/panoptes/bin/update-dns-ip.sh 2>&1 >> /var/panoptes/logs/update-ip.sh
+
 _DEVICE=${1:-enp0s25}
 _DOMAIN="$(hostname)"
 _HOSTKEY="${DNS_HOSTKEY}"
+# _HOSTKEY="${DNS_HOSTKEY:-KEY_GOES_HERE}"
+
+# Sleep if coming from cron so network can start
+SLEEP_ME="${SLEEP_TIME:-0}"
+sleep "${SLEEP_ME}"
+
 _IP=$(ip address show ${_DEVICE} | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
 
 date +%F
