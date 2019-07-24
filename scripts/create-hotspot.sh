890f4b22d16dc90b02ad9cba8fe6524bea25ba82
Wilfred Gee <wtylergee@gmail.com>
2019-07-23 16:18:22 -1000

    Add better docs

--- scripts/create-hotspot.sh
+++ scripts/create-hotspot.sh
@@ -1,7 +1,42 @@
-#!/bin/bash -e
+#!/bin/bash -ie
 
-HOTSPOT_NAME="PanoptesHotspot"
-SSID_NAME="panoptes-net"
+usage() {
+  echo -n "##################################################
+# Create a local hotspot for devices to connect to.
+#
+##################################################
+
+ $ $(basename $0)
+
+ This script currently will read the environment variables
+ $HOTSPOT_NAME and $HOTSPOT_PASS. If $HOTSPOT_PASS is not provided
+ the script will prompt for user input. If using as a script, pass
+ the environment variables when calling, as shown in examples below.
+
+ Examples:
+
+    # Start hotspot with default options, ask for password.
+    scripts/create-hotspot.sh
+
+    # Start hotspot with default options, set password.
+    HOTSPOT_PASS="s3cr3t_p4ss" scripts/create-hotspot.sh
+
+    # Start hotspot with custom name, set password.
+    HOTSPOT_NAME="WifiName" HOTSPOT_PASS="s3cr3t_p4ss" scripts/create-hotspot.sh
+
+    # Start hotspot with custom ssid, ask for password.
+    SSID_NAME="pan000-wifi" scripts/create-hotspot.sh
+"
+}
+
+START=${1:-help}
+if [ "${START}" = 'help' ] || [ "${START}" = '-h' ] || [ "${START}" = '--help' ]; then
+    usage
+    exit 1
+fi
+
+HOTSPOT_NAME="${HOTSPOT_NAME:-PanoptesHotspot}"
+SSID_NAME="${SSID_NAME:-panoptes-net}"
 
 if [[ -z "${HOTSPOT_PASS}" ]]; then
     #statements
