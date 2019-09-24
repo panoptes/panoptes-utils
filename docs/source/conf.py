f832c2cd2678bf7361953b7d50322ba55e1f5dea
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-09-24 10:37:09 +1000

    Adding default matplotlib modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -214,3 +214,8 @@ intersphinx_mapping = {
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+doctest_global_setup = """
+from matlotlib import pyplot as plt
+import numpy as np
+"""
