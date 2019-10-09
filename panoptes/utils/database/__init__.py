e17cdce829bb15fa9b2e86d0354ce8d8d7759d79
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-10-10 08:37:23 +1100

    Logger updates

--- panoptes/utils/database/__init__.py
+++ panoptes/utils/database/__init__.py
@@ -50,7 +50,8 @@ class AbstractPanDB(metaclass=abc.ABCMeta):
             logger: (Optional) logger to use for warnings.
         """
         self.logger = logger
-        self.logger.info(f'Creating PanDB {db_name} with collections: {collection_names}')
+        if self.logger:
+            self.logger.info(f'Creating PanDB {db_name} with collections: {collection_names}')
         self.db_name = db_name
         self.collection_names = collection_names
 
