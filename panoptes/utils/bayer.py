7686211c35031f16902ec6b0579da1976bbf9ae0
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-11-27 10:38:38 -1000

    Some updates for color channels (#78)

--- panoptes/utils/bayer.py
+++ panoptes/utils/bayer.py
@@ -6,186 +6,139 @@ from decimal import Decimal
 from decimal import ROUND_HALF_UP
 
 
-def get_rgb_cube(cube):
-    """ Given a cube of data, return the same cube split by RGB """
+def get_rgb_data(data, separate_green=False):
+    """Get the data split into separate channels for RGB.
+    
+    `data` can be a 2D (`W x H`) or 3D (`N x W x H`) array where `W`=width
+    and `H`=height of the data, with `N`=number of frames.
+    
+    The return array will be a `3 x W x H` or `3 x N x W x H` array.
+    
+    The Bayer array defines a superpixel as a collection of 4 pixels
+    set in a square grid:
 
-    stamp_side = int(np.sqrt(cube[0].shape))
-    # Get the masks
-    first_frame = cube[0].reshape(stamp_side, stamp_side)
-    rgb_masks = np.array([m for m in get_rgb_masks(first_frame).values()])
+                     R G
+                     G B
 
-    def mask_frame(f):
-        r = np.ma.array(f, mask=~rgb_masks[0])
-        g = np.ma.array(f, mask=~rgb_masks[1])
-        b = np.ma.array(f, mask=~rgb_masks[2])
-        return np.ma.array([r, g, b])
+    `ds9` and other image viewers define the coordinate axis from the
+    lower left corner of the image, which is how a traditional x-y plane
+    is defined and how most images would expect to look when viewed. This
+    means that the `(0, 0)` coordinate position will be in the lower left
+    corner of the image.
 
-    rgb_cube = np.apply_along_axis(mask_frame, 1, cube)
+    When the data is loaded into a `numpy` array the data is flipped on the
+    vertical axis in order to maintain the same indexing/slicing features.
+    This means the the `(0, 0)` coordinate position is in the upper-left
+    corner of the array when output. When plotting this array one can use
+    the `origin='lower'` option to view the array as would be expected in
+    a normal image although this does not change the actual index.
 
-    # Rearrange so the color is first
-    r = rgb_cube[:, 0, :]
-    g = rgb_cube[:, 1, :]
-    b = rgb_cube[:, 2, :]
+    Note:
 
-    return np.ma.array([r, g, b])
+        Image dimensions:
+
+         ----------------------------
+         x | width  | i | columns |  5208
+         y | height | j | rows    |  3476
+
+        Bayer Pattern (as seen in ds9):
+
+                                      x / j
+
+                      0     1    2     3 ... 5204 5205 5206 5207
+                    --------------------------------------------
+               3475 |  R   G1    R    G1        R   G1    R   G1
+               3474 | G2    B   G2     B       G2    B   G2    B
+               3473 |  R   G1    R    G1        R   G1    R   G1
+               3472 | G2    B   G2     B       G2    B   G2    B
+                  . |
+         y / i    . |
+                  . |
+                  3 |  R   G1    R    G1        R   G1    R   G1
+                  2 | G2    B   G2     B       G2    B   G2    B
+                  1 |  R   G1    R    G1        R   G1    R   G1
+                  0 | G2    B   G2     B       G2    B   G2    B
 
 
-def get_rgb_data(data, **kwargs):
-    rgb_masks = get_rgb_masks(data, **kwargs)
+        Note: This therefore assumes the data is in the following format:
 
-    assert rgb_masks is not None
+                 | row (y) |  col (x)
+             --------------| ------
+              R  |  odd i, |  even j
+              G1 |  odd i, |   odd j
+              G2 | even i, |  even j
+              B  | even i, |   odd j
+              
+        Or, in other words, the bottom-left (i.e. `(0,0)`) super-pixel is an RGGB pattern.
 
+        And a mask can therefore be generated as:
+
+            bayer[1::2, 0::2] = 1 # Red
+            bayer[1::2, 1::2] = 1 # Green
+            bayer[0::2, 0::2] = 1 # Green
+            bayer[0::2, 1::2] = 1 # Blue
+            
+    """
+    rgb_masks = get_rgb_masks(data, separate_green=separate_green)
+    
     color_data = list()
-
-    r_data = np.ma.array(data, mask=~rgb_masks['r'])
-    color_data.append(r_data)
-
-    g_data = np.ma.array(data, mask=~rgb_masks['g'])
-    color_data.append(g_data)
-
-    try:
-        c_data = np.ma.array(data, mask=~rgb_masks['c'])
-        color_data.append(c_data)
-    except KeyError:
-        pass
-
-    b_data = np.ma.array(data, mask=~rgb_masks['b'])
-    color_data.append(b_data)
+    
+    # Red
+    color_data.append(np.ma.array(data, mask=rgb_masks[0]))
+    
+    # Green
+    color_data.append(np.ma.array(data, mask=rgb_masks[1]))
+    
+    if separate_green:
+        color_data.append(np.ma.array(data, mask=rgb_masks[2]))
+    
+    # Blue
+    color_data.append(np.ma.array(data, mask=rgb_masks[-1]))
+    
 
     return np.ma.array(color_data)
 
 
-def get_rgb_masks(data, separate_green=False, mask_path=None, force_new=False, verbose=False):
+def get_rgb_masks(data, separate_green=False):
     """Get the RGGB Bayer pattern for the given data.
+    
+    See `get_rgb_data` for description of data.
 
     Args:
         data (`numpy.array`): An array of data representing an image.
         separate_green (bool, optional): If the two green channels should be separated,
             default False.
-        mask_path (str, optional): Path to file to save/lookup mask.
-        force_new (bool, optional): If a new file should be generated, default False.
-        verbose (bool, optional): Verbose, default False.
 
     Returns:
-        TYPE: Description
+        tuple(np.array, np.array, np.array): A 3-tuple of numpy arrays of `bool` type.
     """
-    if mask_path is None:
-        mask_path = os.path.join(os.environ['PANDIR'],
-                                 f'rgb_masks_{data.shape[0]}_{data.shape[1]}.npz')
+    
+    r_mask = np.ones_like(data).astype(bool)
+    g1_mask = np.ones_like(data).astype(bool)
+    b_mask = np.ones_like(data).astype(bool)
+    
+    if separate_green:
+        g2_mask = np.ones_like(data).astype(bool)
+    else:
+        g2_mask = g1_mask
+    
+    if data.ndim == 2:
+        r_mask[1::2, 0::2] = False
+        g1_mask[1::2, 1::2] = False
+        g2_mask[0::2, 0::2] = False
+        b_mask[0::2, 1::2] = False
+    elif data.ndim == 3:
+        r_mask[..., 1::2, 0::2] = False
+        g1_mask[..., 1::2, 1::2] = False
+        g2_mask[..., 0::2, 0::2] = False
+        b_mask[..., 0::2, 1::2] = False
+    else:
+        raise Exception('Only 2D and 3D data allowed')
 
-    def _print(msg):
-        if verbose:
-            print(msg)
-
-    _print('Mask path: {}'.format(mask_path))
-
-    if force_new:
-        _print("Forcing a new mask file")
-        try:
-            os.remove(mask_path)
-        except FileNotFoundError:
-            pass
-
-    # Try to load existing file and if not generate new
-    try:
-        loaded_masks = np.load(mask_path)
-        _print("Loaded masks")
-        mask_shape = loaded_masks[loaded_masks.files[0]].shape
-        if mask_shape != data.shape:
-            _print("Removing mask with wrong size")
-            os.remove(mask_path)
-            raise FileNotFoundError
-        else:
-            _print("Using saved masks")
-            _rgb_masks = {color: loaded_masks[color] for color in loaded_masks.files}
-    except Exception:
-        _print("Making RGB masks")
-
-        if data.ndim > 2:
-            data = data[0]
-
-        w, h = data.shape
-
-        # See the docstring for `pixel_color` for full description of indexing values.
-
-        #        |   row   |  col
-        #    --------------| ------
-        #     R  |  odd i, | even j
-        #     G1 |  odd i, |  odd j
-        #     G2 | even i, | even j
-        #     B  | even i, |  odd j
-
-        def is_red(pos):
-            return pos[0] % 2 == 1 and pos[1] % 2 == 0
-
-        def is_blue(pos):
-            return pos[0] % 2 == 0 and pos[1] % 2 == 1
-
-        def is_g1(pos):
-            return pos[0] % 2 == 1 and pos[1] % 2 == 1
-
-        def is_g2(pos):
-            return pos[0] % 2 == 0 and pos[1] % 2 == 0
-
-        red_mask = (np.array(
-            [
-                is_red(index)
-                for index, _
-                in np.ndenumerate(data)
-            ]
-        ).reshape(w, h))
-
-        blue_mask = (np.array(
-            [
-                is_blue(index)
-                for index, _
-                in np.ndenumerate(data)
-            ]
-        ).reshape(w, h))
-
-        if separate_green:
-            _print("Making separate green masks")
-            green1_mask = (np.array(
-                [
-                    is_g1(index)
-                    for index, _
-                    in np.ndenumerate(data)
-                ]
-            ).reshape(w, h))
-
-            green2_mask = (np.array(
-                [
-                    is_g2(index)
-                    for index, _
-                    in np.ndenumerate(data)
-                ]
-            ).reshape(w, h))
-
-            _rgb_masks = {
-                'r': red_mask,
-                'g': green1_mask,
-                'c': green2_mask,
-                'b': blue_mask,
-            }
-        else:
-            green_mask = (np.array(
-                [
-                    is_g1(index) or is_g2(index)
-                    for index, _ in
-                    np.ndenumerate(data)
-                ]
-            ).reshape(w, h))
-
-            _rgb_masks = {
-                'r': red_mask,
-                'g': green_mask,
-                'b': blue_mask,
-            }
-
-        _print("Saving masks files")
-        np.savez_compressed(mask_path, **_rgb_masks)
-
-    return _rgb_masks
+    if separate_green:
+        return np.array([r_mask, g1_mask, g2_mask, b_mask])
+    else:
+        return np.array([r_mask, g1_mask, b_mask])
 
 
 def spiral_matrix(A):
@@ -252,7 +205,7 @@ def pixel_color(x, y):
          x | width  | i | columns |  5208
          y | height | j | rows    |  3476
 
-        Bayer Pattern:
+        Bayer Pattern (as seen in ds9):
 
                                       x / j
 
@@ -280,10 +233,10 @@ def pixel_color(x, y):
               G2 | even i, |  even j
               B  | even i, |   odd j
 
-            bayer[1::2, 0::2, 0] = 1 # Red
-            bayer[1::2, 1::2, 1] = 1 # Green
-            bayer[0::2, 0::2, 1] = 1 # Green
-            bayer[0::2, 1::2, 2] = 1 # Blue
+            bayer[1::2, 0::2] = 1 # Red
+            bayer[1::2, 1::2] = 1 # Green
+            bayer[0::2, 0::2] = 1 # Green
+            bayer[0::2, 1::2] = 1 # Blue
 
     Returns:
         str: one of 'R', 'G1', 'G2', 'B'
