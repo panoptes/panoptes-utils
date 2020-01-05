2af3194a5daf744ba6079cee463b6b65619823ea
Wilfred Gee <wtylergee@gmail.com>
2020-01-04 15:52:24 -1000

    Silly change for coverage

--- panoptes/utils/tests/test_image_utils.py
+++ panoptes/utils/tests/test_image_utils.py
@@ -30,7 +30,7 @@ def test_crop_data():
     ones = np.ones((201, 201))
     assert ones.sum() == 40401.
 
-    cropped01 = img_utils.crop_data(ones, verbose=True)
+    cropped01 = img_utils.crop_data(ones, verbose=False)  # False to exercise coverage.
     assert cropped01.sum() == 40000.
 
     cropped02 = img_utils.crop_data(ones, verbose=True, box_width=10)
