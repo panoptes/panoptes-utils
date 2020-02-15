99ffc0183f3ad4393fb422a2b7627a3d70254997
Wilfred Gee <wtylergee@gmail.com>
2020-02-15 13:35:33 +1100

    Pulled in changes from POCS d5cf73755a08a2e2d6ed2e5d5565ac7507c9ed2c

--- panoptes/utils/library.py
+++ panoptes/utils/library.py
@@ -5,7 +5,7 @@ from astropy.utils import resolve_name
 from panoptes.utils import error
 
 
-def load_c_library(name, path=None, logger=None):
+def load_c_library(name, path=None, mode=ctypes.DEFAULT_MODE, logger=None):
     """Utility function to load a shared/dynamically linked library (.so/.dylib/.dll).
 
     The name and location of the shared library can be manually specified with the library_path
@@ -15,15 +15,22 @@ def load_c_library(name, path=None, logger=None):
     Args:
         name (str): name of the library (without 'lib' prefix or any suffixes, e.g. 'fli').
         path (str, optional): path to the library e.g. '/usr/local/lib/libfli.so'.
+        mode (int, optional): mode in which to load the library, see dlopen(3) man page for
+            details. Should be one of ctypes.RTLD_GLOBAL, ctypes.RTLD_LOCAL, or
+            ctypes.DEFAULT_MODE. Default is ctypes.DEFAULT_MODE.
+        logger (logging.Logger, optional): logger to use.
 
     Returns:
         ctypes.CDLL
 
     Raises:
-        panoptes.utils.error.NotFound: raised if library_path not given & find_libary fails to
+        pocs.utils.error.NotFound: raised if library_path not given & find_library fails to
             locate the library.
         OSError: raises if the ctypes.CDLL loader cannot load the library.
     """
+    if mode is None:
+        # Interpret a value of None as the default.
+        mode = ctypes.DEFAULT_MODE
     # Open library
     if logger:
         logger.debug("Opening {} library".format(name))
@@ -32,7 +39,7 @@ def load_c_library(name, path=None, logger=None):
         if not path:
             raise error.NotFound("Cound not find {} library!".format(name))
     # This CDLL loader will raise OSError if the library could not be loaded
-    return ctypes.CDLL(path)
+    return ctypes.CDLL(path, mode=mode)
 
 
 def load_module(module_name):
