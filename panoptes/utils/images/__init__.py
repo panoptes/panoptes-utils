cf81e6fadc62ef1f952929816e9648a83867d3b5
Wilfred Gee <wtylergee@gmail.com>
2020-02-21 11:33:07 +1100

    Trying to get error

--- panoptes/utils/images/__init__.py
+++ panoptes/utils/images/__init__.py
@@ -241,7 +241,7 @@ def _make_pretty_from_cr2(fname, title=None, timeout=15, **kwargs):
         if verbose:
             print(output)
     except Exception as e:
-        raise error.InvalidCommand(f"Error executing {script_name}: {e.stderr!r}\nCommand: {cmd}")
+        raise error.InvalidCommand(f"Error executing {script_name}: {e.output!r}\nCommand: {cmd}")
 
     return fname.replace('cr2', 'jpg')
 
