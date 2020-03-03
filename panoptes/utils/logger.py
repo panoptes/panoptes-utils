71cd338328a4db9c932c6eeca5c962048e1a0ef3
Wilfred Gee <wtylergee@gmail.com>
2020-03-03 09:10:34 -1000

    Logger
    * root logger can take file name or dir.
    * Better docstring.
    * Removed old logger tests.

--- panoptes/utils/logger.py
+++ panoptes/utils/logger.py
@@ -2,23 +2,34 @@ import os
 from loguru import logger
 
 
-def get_root_logger(profile='panoptes', log_dir=None):
+def get_root_logger(profile='panoptes',
+                    log_file='panoptes_{time:YYYYMMDD!UTC}.log',
+                    log_dir=None):
     """Creates a root logger for PANOPTES used by the PanBase object.
+
+    Note: The `log_dir` is determined first from `$PANLOG` if it exists, then
+      `$PANDIR/logs` if `$PANDIR` exists, otherwise defaults to `.`.
 
     Args:
         profile (str, optional): The name of the logger to use, defaults to 'panoptes'.
+        log_file (str|None, optional): The filename, defaults to `panoptes_{time:YYYYMMDD!UTC}.log`.
+        log_dir (str|None, optional): The directory to place the log file, see note.
 
     Returns:
-        `loguru.logger`: A configured instance of the logger
+        `loguru.logger`: A configured instance of the logger.
     """
 
     # Create the directory for the per-run files.
     if log_dir is None:
-        log_dir = os.getenv('PANLOG', os.path.expandvars('$PANDIR/logs'))
+        try:
+            log_dir = os.environ['PANLOG']
+        except KeyError:
+            log_dir = os.path.join(os.getenv('PANDIR', '.'), 'logs')
+    log_dir = os.path.normpath(log_dir)
     os.makedirs(log_dir, exist_ok=True)
 
     # Serialize messages to a file.
-    log_path = os.path.join(log_dir, 'panoptes_{time:YYYYMMDD!UTC}.log')
+    log_path = os.path.normpath(os.path.join(log_dir, log_file))
     logger.add(log_path,
                rotation='11:30',
                enqueue=True,
--- panoptes/utils/tests/test_logger.py
+++ panoptes/utils/tests/test_logger.py
@@ -1,169 +1,20 @@
+import os
 import pytest
 
-from panoptes.utils.logger import field_name_to_key
-from panoptes.utils.logger import logger_msg_formatter
 from panoptes.utils.logger import get_root_logger
-from panoptes.utils.serializers import from_yaml
 
 
-def test_field_name_to_key():
-    assert not field_name_to_key('.')
-    assert not field_name_to_key('[')
-    assert field_name_to_key('abc') == 'abc'
-    assert field_name_to_key(' abc ') == ' abc '
-    assert field_name_to_key('abc.def') == 'abc'
-    assert field_name_to_key('abc[1].def') == 'abc'
+def test_root_logger(caplog, tmp_path):
+    logger = get_root_logger(log_dir=str(tmp_path))
+    logger.debug('Hi')
+    assert os.listdir(tmp_path)[0].startswith('panoptes_')
+    assert caplog.records[-1].message == 'Hi'
+    assert caplog.records[-1].levelname == 'DEBUG'
 
-
-def test_logger_msg_formatter_1_dict():
-    d = dict(abc='def', xyz=123)
-
-    tests = [
-        # Single anonymous reference, satisfied by the entire dict.
-        ('{}', "{'abc': 'def', 'xyz': 123}"),
-
-        # Single anonymous reference, satisfied by the entire dict.
-        ('{!r}', "{'abc': 'def', 'xyz': 123}"),
-
-        # Position zero references, satisfied by the entire dict.
-        ('{0} {0}', "{'abc': 'def', 'xyz': 123} {'abc': 'def', 'xyz': 123}"),
-
-        # Reference to a valid key in the dict.
-        ('{xyz}', "123"),
-
-        # Invalid modern reference, so %s format applied.
-        ('%s {1}', "{'abc': 'def', 'xyz': 123} {1}"),
-
-        # Valid legacy format applied to whole dict.
-        ('%r', "{'abc': 'def', 'xyz': 123}"),
-        ('%%', "%"),
-    ]
-
-    for fmt, msg in tests:
-        assert logger_msg_formatter(fmt, d) == msg, fmt
-
-    # Now tests with entirely invalid formats, so warnings should be issued.
-    tests = [
-        '%(2)s',
-        '{def}',
-        '{def',
-        'def}',
-        '%d',
-        # Bogus references either way.
-        '{0} {1} %(2)s'
-    ]
-
-    for fmt in tests:
-        with pytest.warns(UserWarning):
-            assert logger_msg_formatter(fmt, d) == fmt
-
-
-def test_logger_msg_formatter_1_non_dict():
-    d = ['abc', 123]
-
-    tests = [
-        # Single anonymous reference, satisfied by first element.
-        ('{}', "abc"),
-
-        # Single anonymous reference, satisfied by first element.
-        ('{!r}', "'abc'"),
-
-        # Position references, satisfied by elements.
-        ('{1} {0!r}', "123 'abc'"),
-
-        # Valid modern reference, %s ignored.
-        ('%s {1}', "%s 123"),
-
-        # Valid legacy format applied to whole list.
-        ('%r', "['abc', 123]"),
-
-        # Valid legacy format applied to whole list.
-        ('%s', "['abc', 123]"),
-    ]
-
-    for fmt, msg in tests:
-        assert logger_msg_formatter(fmt, d) == msg, fmt
-
-    # Now tests with entirely invalid formats, so warnings should be issued.
-    tests = [
-        # We only have two args, so a reference to a third should fail.
-        '{2}',
-        '%(2)s',
-        # Unknown key
-        '{def}',
-        '%(def)s',
-        # Malformed key
-        '{2',
-        '{',
-        '2}',
-        '}',
-        '{}{}{}',
-        '%d',
-    ]
-
-    for fmt in tests:
-        with pytest.warns(UserWarning):
-            assert logger_msg_formatter(fmt, d) == fmt
-
-
-def test_root_logger():
-    logger = get_root_logger()
-    logger.info('Hi')
-    logger.info('Hi', extra=dict(foo='bar'))
-
-
-def test_root_logger_with_config():
-    config = from_yaml(TEST_CONFIG)
-    logger = get_root_logger(log_config=config)
-    logger.info('With config')
-
-
-def test_root_logger_with_config_utc():
-    config = from_yaml(TEST_CONFIG)
-    config['use_utc'] = False
-    logger = get_root_logger(log_config=config)
-    logger.info('Without utc')
-
-
-TEST_CONFIG = """
-version: 1
-use_utc: True
-formatters:
-  simple:
-    format: '%(asctime)s - %(message)s'
-    datefmt: '%H:%M:%S'
-  detail:
-    style: '{'
-    format: '{levelname:.1s}{asctime}.{msecs:03.0f} {filename:>25s}:{lineno:03d}] {message}'
-    datefmt: '%m%d %H:%M:%S'
-handlers:
-  all:
-    class: logging.handlers.TimedRotatingFileHandler
-    level: DEBUG
-    formatter: simple
-    when: W6
-    backupCount: 4
-  info:
-    class: logging.StreamHandler
-    level: INFO
-    formatter: simple
-  error:
-    class: logging.handlers.TimedRotatingFileHandler
-    level: ERROR
-    formatter: detail
-    when: W6
-    backupCount: 4
-loggers:
-  all:
-    handlers: [all]
-    propagate: true
-  info:
-    handlers: [info]
-    propagate: true
-  error:
-    handlers: [error]
-    propagate: true
-root:
-  level: DEBUG
-  handlers: [all, info]
-"""
+    os.environ['PANLOG'] = str(tmp_path)
+    logger = get_root_logger(log_file='foo.log')
+    logger.info('Bye', extra=dict(foo='bar'))
+    assert len(os.listdir(tmp_path)) == 2
+    assert os.listdir(tmp_path)[-1] == 'foo.log'
+    assert caplog.records[-1].message == 'Bye'
+    assert caplog.records[-1].levelname == 'INFO'
