cbcd9bb69862cd0be06af4e2699073a70cccacd4
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-03-05 10:55:54 -1000

    Removing POCS logger (#134)

--- panoptes/utils/time.py
+++ panoptes/utils/time.py
@@ -159,7 +159,7 @@ class CountdownTimer(object):
     def restart(self):
         """Restart the timed duration."""
         self.target_time = time.monotonic() + self.duration
-        logger.debug(f'Restarting {self}')
+        logger.trace(f'Restarting {self}')
 
     def sleep(self, max_sleep=None):
         """Sleep until the timer expires, or for max_sleep, whichever is sooner.
@@ -180,7 +180,7 @@ class CountdownTimer(object):
             assert max_sleep > 0
             sleep_time = max_sleep
 
-        logger.debug(f'Sleeping for {sleep_time:.02f} seconds')
+        logger.trace(f'Sleeping for {sleep_time:.02f} seconds')
         time.sleep(sleep_time)
 
         return sleep_time < remaining
