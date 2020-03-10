c4110f7e2db7f93e3cbf3c4f937dccdeddca3a2e
Preethi <54646323+preethi524@users.noreply.github.com>
2020-03-10 12:24:09 -1000

    Update fits.py
    Fixing blanks

--- panoptes/utils/images/fits.py
+++ panoptes/utils/images/fits.py
@@ -108,9 +108,10 @@ def get_solve_field(fname, replace=True, remove_extras=True, **kwargs):
     >>> ra = 15.23
     >>> dec = 90
     >>> fits_utils.get_solve_field(fits_fn, ra=ra, dec=dec)
-
+    ...
     >>> radius = 5 # deg
     >>> fits_utils.solve_field(fits_fn, radius=radius)
+    ...
 
     Args:
         fname ({str}): Name of FITS file to be solved
