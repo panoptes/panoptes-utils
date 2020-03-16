ced0fa31dfb51830b9ff1d6e68e743a11c536142
Wilfred Gee <wtylergee@gmail.com>
2020-03-16 07:13:50 -1000

    Fetch the CR2 file for testing.

--- conftest.py
+++ conftest.py
@@ -380,7 +380,7 @@ def noheader_fits_file(data_dir):
 
 @pytest.fixture(scope='session')
 def cr2_file():
-    cr2_path = '/data/canon.cr2'
+    cr2_path = os.path.join(data_dir, 'canon.cr2')
 
     if not os.path.exists(cr2_path):
         pytest.skip("No CR2 file found, skipping test.")
