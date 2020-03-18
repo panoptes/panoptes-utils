6ba5a79132ae74947018f20a32bb1808f1458e53
Wilfred Gee <wtylergee@gmail.com>
2020-03-17 18:55:33 -1000

    More simplifying of docker

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 SOURCE_DIR="${PANDIR}/panoptes-utils"
-BASE_CLOUD_FILE="cloudbuild-base-${1:-all}.yaml"
+BASE_CLOUD_FILE="cloudbuild.yaml"
 
 cd "${SOURCE_DIR}"
 
 echo "Using ${BASE_CLOUD_FILE}"
 echo "Building panoptes-utils!"
 gcloud builds submit \
-    --timeout="5h" \
     --config "${SOURCE_DIR}/docker/${BASE_CLOUD_FILE}" \
     "${SOURCE_DIR}"
