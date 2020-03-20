b8605fcade47b97c68cef45b2ac725de2c1ed4b0
Wilfred Gee <wtylergee@gmail.com>
2020-03-20 13:51:00 -1000

    Try better if

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,14 +3,14 @@
 USER_ID=${LOCAL_USER_ID:-1000}
 
 # See https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
-if [[ ${USER_ID} != 1000 ]]; then
+if [ "${USER_ID}" != 1000 ]; then
     echo "Starting with UID : $USER_ID"
-    useradd --shell /bin/zsh -u $USER_ID -o -c "PANOPTES User" -m -G plugdev,dialout,panoptes
+    useradd --shell /bin/zsh -u "${USER_ID}" -o -c "PANOPTES User" -m -G plugdev,dialout,panoptes
     export HOME=/home/panoptes
 fi
 
 # Authenticate if key has been set - used on local units
-if [ ! -z ${GOOGLE_APPLICATION_CREDENTIALS} ]; then
+if [ -n "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
     echo "Found Google credentials, activating service account."
     echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
     gosu panoptes gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
