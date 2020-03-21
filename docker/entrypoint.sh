4bf3be65b7d37a8da60ef73ab6ad2c4c4bc7bf10
Wilfred Gee <wtylergee@gmail.com>
2020-03-20 15:15:01 -1000

    Change panoptes userid to mach that of docker user.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/bash -ie
 
-USER=panoptes
 USER_ID=${LOCAL_USER_ID:-1000}
 
 # See https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
 if [ "${USER_ID}" != 1000 ]; then
     echo "Starting with UID : $USER_ID"
-    useradd --shell /bin/zsh --uid "${USER_ID}" -c "PANOPTES Docker User" -MN -G plugdev,dialout,panoptes panoptes-docker
-    export HOME=/home/panoptes
-    USER="panoptes-docker"
+    # Modify panoptes default group.
+    usermod --gid "${USER_ID}"
+    # Change permissions
+    chown -R "${USER_ID}:${USER_ID}" "${PANDIR}"
+    chown -R "${USER_ID}:${USER_ID}" "/home/panoptes"
 fi
 
 # Authenticate if key has been set - used on local units
 if [ -n "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
     echo "Found Google credentials, activating service account."
     echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
-    gosu "${USER}" gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+    gosu panoptes gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 fi
 
 # Pass arguments
-exec gosu "${USER}" "$@"
+exec gosu panoptes "$@"
 
