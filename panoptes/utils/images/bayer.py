f5f8bbd3ff19aff5d932754af2c3a36a7f60f537
Wilfred Gee <wtylergee@gmail.com>
2020-03-22 16:08:19 -1000

    Change scope of fixtures

--- panoptes/utils/images/bayer.py
+++ panoptes/utils/images/bayer.py
@@ -184,11 +184,13 @@ def get_rgb_background(fits_fn,
     Most of the options are described in the `photutils.Background2D` page:
     https://photutils.readthedocs.io/en/stable/background.html#d-background-and-noise-estimation
 
-    >>> fits_fn = getfixture('solved_fits_file')
     >>> from panoptes.utils.images import fits as fits_utils
+    >>> fits_fn = getfixture('solved_fits_file')
+
     >>> data = fits_utils.getdata(fits_fn)
     >>> data.mean()
     2236.816...
+
     >>> rgb_back = get_rgb_background(fits_fn)
     >>> rgb_back.mean()
     2202.392...
