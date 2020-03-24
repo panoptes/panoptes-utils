39017fd51a167d5fe848fd41742556262592aac1
Preethi <54646323+preethi524@users.noreply.github.com>
2020-03-23 14:20:35 -1000

    Update fits.py
    Moved header check to be within docstring

--- panoptes/utils/images/fits.py
+++ panoptes/utils/images/fits.py
@@ -476,14 +476,14 @@ def write_fits(data, header, filename, logger=None, exposure_event=None):
 
 def update_observation_headers(file_path, info):
     with fits.open(file_path, 'update') as f:
-
-        # Let us check the headers
+    """Check the headers
+        >>> from panoptes.utils.images import fits as fits_utils
         >>> fits_fn = getfixture('unsolved_fits_file')
         >>> info = {'field_name': 'M42'}
         >>> update_observation_headers{fits_fn, info}
         >>> fits_utils.getval(fits_fn, 'FIELD')
         M42
-
+    """
         hdu = f[0]
         hdu.header.set('IMAGEID', info.get('image_id', ''))
         hdu.header.set('SEQID', info.get('sequence_id', ''))
