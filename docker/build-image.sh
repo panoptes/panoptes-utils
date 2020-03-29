7331c4bf0a27907bb5552664658b8812c988e5a1
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-03-29 11:47:40 -1000

    Allow for tagged docker versions (#165)
    * Remove activate from $GOOGLE_APPLICATION_CREDENTIALS. If it is properly set as an env var then any of the google client will recognize it automatically.
    * Allow for specifying docker image tag.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
+
+TAG=${1:-latest}
+
 SOURCE_DIR="${PANDIR}/panoptes-utils"
 BASE_CLOUD_FILE="cloudbuild.yaml"
 
 cd "${SOURCE_DIR}"
 
 echo "Using ${BASE_CLOUD_FILE}"
-echo "Building panoptes-utils"
+echo "Building panoptes-utils:${TAG}"
 gcloud builds submit \
     --timeout="5h" \
+    --substitutions=_TAG="${TAG}" \
     --config "${SOURCE_DIR}/docker/${BASE_CLOUD_FILE}" \
     "${SOURCE_DIR}"
