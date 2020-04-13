e76f99ebc0fa3c3d855cdf0f73126a4ca564e975
Wilfred Gee <wtylergee@gmail.com>
2020-04-12 16:37:30 -1000

    * Better note on fake password.

--- resources/jupyter/jupyter_notebook_config.py
+++ resources/jupyter/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 # Configuration file for jupyter-notebook.
 
-# Default `panoptes` password. Note that this is note for security purposes.
+# Default `panoptes` password. Note that this is not for security purposes but simply to allow access.
 c.NotebookApp.password = u'sha1:5da068d1d49a:db1c45f26263c718d461e708072f2a76d2f64e4b'  # noqa
 
 # Answer yes to any prompts.
