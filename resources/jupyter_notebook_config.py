3ae8240dcff4c45302feeba5f05393a13e387719
Wilfred Gee <wtylergee@gmail.com>
2020-04-12 15:38:01 -1000

    Add some `noqa` so linting passes.

--- resources/jupyter_notebook_config.py
+++ resources/jupyter_notebook_config.py
@@ -1,9 +1,8 @@
-import os
 # Configuration file for jupyter-notebook.
 
 # Default `panoptes` password.
-c.NotebookApp.password = u'sha1:5da068d1d49a:db1c45f26263c718d461e708072f2a76d2f64e4b'
+c.NotebookApp.password = u'sha1:5da068d1d49a:db1c45f26263c718d461e708072f2a76d2f64e4b'  # noqa
 
 # Answer yes to any prompts.
-c.JupyterApp.answer_yes = True
-c.NotebookApp.open_browser = False
+c.JupyterApp.answer_yes = True  # noqa
+c.NotebookApp.open_browser = False  # noqa
