cfa2e08e9a7225e43bfc2f452df407bd5a2be1ce
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-04-29 07:20:41 -1000

    Fix minutes stats.

--- panoptes/utils/data/__init__.py
+++ panoptes/utils/data/__init__.py
@@ -233,7 +233,7 @@ def search_observations(
              Wasp 35         69
     Name: num_images, dtype: Int64
     >>> search_results.total_minutes_exptime.sum()
-    1420.0
+    1422.0
 
     Args:
         ra (float|None): The RA position in degrees of the center of search.
