efdc8bef077e0e6360dff38de6c51584ab7efce5
Wilfred Gee <wtylergee@gmail.com>
2020-06-06 15:52:41 -1000

    Testing
    * Trying to fix up the coverage.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+REPORT_FILE=${REPORT_FILE:-coverage.xml}
+
 export PYTHONPATH="${PYTHONPATH}:/var/panoptes/panoptes-utils/scripts/testing/coverage"
 export COVERAGE_PROCESS_START="/var/panoptes/panoptes-utils/setup.cfg"
 
@@ -8,5 +10,11 @@ coverage erase
 # Run coverage over the pytest suite
 echo "Starting tests"
 coverage run "$(command -v pytest)"
+
+echo "Combining coverage"
+coverage combine
+
+echo "Making XML coverage report at ${REPORT_FILE}"
+coverage xml -o "${REPORT_FILE}"
 
 exit 0
