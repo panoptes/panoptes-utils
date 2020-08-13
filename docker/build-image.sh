4f1805c7929ca8821d8c036301655040d55fbceb
Wilfred Gee <wtylergee@gmail.com>
2020-08-13 10:01:57 -1000

    * Increasing machine size for cloud build and specifying other options in cloudbuild file rather than build script. * Allow for installing from different github branches.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -3,14 +3,14 @@ set -e
 
 TAG="${1:-latest}"
 
-SOURCE_DIR="${PANDIR}/panoptes-utils"
-BASE_CLOUD_FILE="cloudbuild.yaml"
+IMAGE_NAME="panoptes-utils"
+CLOUD_FILE="cloudbuild.yaml"
+SOURCE_DIR="${PANDIR}/${IMAGE_NAME}"
 
 cd "${SOURCE_DIR}"
 
-echo "Building gcr.io/panoptes-exp/panoptes-utils:${TAG} in ${SOURCE_DIR}"
+echo "Building gcr.io/panoptes-exp/${IMAGE_NAME}:${TAG} in ${SOURCE_DIR}"
 gcloud builds submit \
-  --timeout="5h" \
   --substitutions="_TAG=${TAG}" \
-  --config "${SOURCE_DIR}/docker/${BASE_CLOUD_FILE}" \
+  --config "${SOURCE_DIR}/docker/${CLOUD_FILE}" \
   "${SOURCE_DIR}"
