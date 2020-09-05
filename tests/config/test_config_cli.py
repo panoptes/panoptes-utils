d72fbb83a5be6afa325ccd6472b20af630cdabd0
Wilfred Gee <wtylergee@gmail.com>
2020-09-05 13:26:28 -1000

    * Testing cli server only starts server. * Get/set from cli works against normal conftest.

--- tests/config/test_config_cli.py
+++ tests/config/test_config_cli.py
@@ -1,6 +1,3 @@
-import time
-from multiprocessing import Process
-
 import pytest
 from click.testing import CliRunner
 from panoptes.utils.config.cli import config_server_cli
@@ -19,39 +16,27 @@ def dummy_cli_port():
 
 @pytest.fixture(scope='module')
 def server(runner, config_path, dummy_cli_port):
-    def start_server():
-        result = runner.invoke(config_server_cli,
-                               [
-                                   'run',
-                                   '--config-file', f'{config_path}',
-                                   '--port', dummy_cli_port,
-                                   '--no-save',
-
-                                   '--ignore-local'
-                               ])
-        assert result.exit_code == 0
-
-    proc = Process(target=start_server)
-    proc.start()
-    yield proc
-    proc.kill()
-
-
-def test_config_server_cli(server, runner, dummy_cli_port):
-    # Let the server start.
-    time.sleep(2)
-    assert server.is_alive()
-
     result = runner.invoke(config_server_cli,
-                           ['get', '--key', f'name', '--port', f'{dummy_cli_port}'])
+                           [
+                               'run',
+                               '--config-file', f'{config_path}',
+                               '--port', dummy_cli_port,
+                               '--no-save',
+                               '--ignore-local'
+                           ])
+    assert result.exit_code == 0
+
+
+def test_config_server_cli(runner, config_port):
+    result = runner.invoke(config_server_cli, ['get', '--verbose', '--key', f'name', '--port', f'{config_port}'])
     assert result.exit_code == 0
     # Ugh. I hate this. Logger is interfering in annoying ways.
     assert result.stdout.endswith("Testing PANOPTES Unit\n")
 
-    result = runner.invoke(config_server_cli, ['set', '--verbose', '--port', f'{dummy_cli_port}', f'name', f'foobar'])
+    result = runner.invoke(config_server_cli, ['set', '--verbose', '--port', f'{config_port}', f'name', f'foobar'])
     assert result.exit_code == 0
     assert result.stdout.endswith("\n{'name': 'foobar'}\n")
 
-    result = runner.invoke(config_server_cli, ['get', '--key', f'name', '--port', f'{dummy_cli_port}'])
+    result = runner.invoke(config_server_cli, ['get', '--key', f'name', '--port', f'{config_port}'])
     assert result.exit_code == 0
     assert result.stdout.endswith("\nfoobar\n")
