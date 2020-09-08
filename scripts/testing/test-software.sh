607ac71f0487dda03eebf1c2c0a780de8f451f1a
Wilfred Gee <wtylergee@gmail.com>
2020-09-08 11:14:08 -1000

    * Make sure there are write permissions for all users on the `build` dir.

--- scripts/testing/test-software.sh
+++ scripts/testing/test-software.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 clear
 
+BUILD_DIR="${BUILD_DIR:-../../build}"
 PANLOG="${PANLOG:-/var/panoptes/logs}"
 
 cat <<EOF
@@ -20,5 +23,8 @@ SLEEP_TIME=${1:-5}
 sleep "${SLEEP_TIME}"
 
 # Create a directory for storing log files and coverage reports.
-mkdir -p build/
+mkdir -p "${BUILD_DIR}" && chmod 777 "${BUILD_DIR}"
 docker-compose -f docker/docker-compose-testing.yaml up
+
+echo "Build dir output"
+ls -la "${BUILD_DIR}"
