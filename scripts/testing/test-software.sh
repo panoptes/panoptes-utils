70a1e38116bc851312bd3196291acb55c8af9b1b
Wilfred Gee <wtylergee@gmail.com>
2020-09-07 14:28:28 -1000

    * Attemping to fix CI testing.

--- scripts/testing/test-software.sh
+++ scripts/testing/test-software.sh
@@ -6,12 +6,9 @@ cat <<EOF
 Beginning test of panoptes-utils software. This software is run inside a virtualized docker
 container that has all of the required dependencies installed.
 
-This will start a single docker container, mapping the host PANDIR=${PANDIR} into the running docker
-container, which allows for testing of any local changes.
-
 You can view the output for the tests in a separate terminal:
 
-tail -F ${PANDIR}/logs/panoptes-testing.log
+tail -F panoptes-testing.log
 
 Tests will begin in 5 seconds. Press Ctrl-c to cancel.
 EOF
