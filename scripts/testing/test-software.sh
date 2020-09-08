103db99d1bec5d72597f1a5b68b156b0ade94843
Wilfred Gee <wtylergee@gmail.com>
2020-09-08 12:26:06 -1000

    * Remove old build dir options.

--- scripts/testing/test-software.sh
+++ scripts/testing/test-software.sh
@@ -19,12 +19,5 @@ SLEEP_TIME=${1:-5}
 
 sleep "${SLEEP_TIME}"
 
-# Create a directory for storing log files and coverage reports.
-mkdir -p "${BUILD_DIR}" && chmod 777 "${BUILD_DIR}"
-echo "Set ${BUILD_DIR}"
-ls -la .
 docker-compose -f docker/docker-compose-testing.yaml up
-
-echo "Build dir output"
-ls -la .
-ls -la "${BUILD_DIR}"
+ls -laR ./tmp
