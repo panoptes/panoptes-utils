d91fd987df766bb8da2ad8d1d4c8798ba9bf2d92
Wilfred Gee <wtylergee@gmail.com>
2020-09-08 17:18:33 -1000

    Run the wait-for-it script from inside `run-tests`, which is running inside the actual test contain.er

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -8,6 +8,9 @@ export COVERAGE_PROCESS_START="/var/panoptes/panoptes-utils/setup.cfg"
 
 coverage erase
 
+echo "Checking to make sure panoptes-config-server is running"
+scripts/wait-for-it.sh panoptes-config-server:8765 -- echo "Found config-server"
+
 # Run coverage over the pytest suite.
 echo "Starting tests"
 coverage run "$(command -v pytest)"
