ca278afcfbb1db144f396e0a67dafc6ee173cb5c
Wilfred Gee <wtylergee@gmail.com>
2020-09-08 17:28:38 -1000

    * Expose ports and make wait script fail.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -9,7 +9,7 @@ export COVERAGE_PROCESS_START="/var/panoptes/panoptes-utils/setup.cfg"
 coverage erase
 
 echo "Checking to make sure panoptes-config-server is running"
-scripts/wait-for-it.sh panoptes-config-server:8765 -- echo "Found config-server"
+scripts/wait-for-it.sh --strict panoptes-config-server:8765 -- echo "Found config-server"
 
 # Run coverage over the pytest suite.
 echo "Starting tests"
