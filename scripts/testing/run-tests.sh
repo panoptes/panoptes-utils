8eeaceabc3eb8f696d0e3843004600e3ff4a7f1f
Wilfred Gee <wtylergee@gmail.com>
2020-09-09 17:19:07 -1000

    * Map local dir.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -9,10 +9,7 @@ export COVERAGE_PROCESS_START="/var/panoptes/panoptes-utils/setup.cfg"
 coverage erase
 
 echo "Checking to make sure panoptes-config-server is running"
-scripts/wait-for-it.sh --timeout 30 \
-  --strict panoptes-config-server:8765 \
-  -- \
-  echo "Found panoptes-config-server, starting tests."
+scripts/wait-for-it.sh --timeout=30 --strict panoptes-config-server:8765 -- echo "Found panoptes-config-server, starting tests."
 
 # Run coverage over the pytest suite.
 coverage run "$(command -v pytest)"
--- tests/config/test_config_cli.py
+++ tests/config/test_config_cli.py
@@ -25,8 +25,8 @@ def test_cli_server(runner, config_path, cli_config_port):
                                    'run',
                                    '--config-file', f'{config_path}',
                                    '--port', cli_config_port,
-                                   '--no-save',
-                                   '--ignore-local'
+                                   '--no-save-local',
+                                   '--no-load-local'
                                ])
         assert result.exit_code == 0
 
@@ -37,7 +37,7 @@ def test_cli_server(runner, config_path, cli_config_port):
     # Let the serve start.
     time.sleep(5)
 
-    result = runner.invoke(config_server_cli, ['--verbose', 'get', f'name', '--port', f'{cli_config_port}'])
+    result = runner.invoke(config_server_cli, ['--verbose', '--port', f'{cli_config_port}', 'get', f'name', ])
     assert result.exit_code == 0
     # Ugh. I hate this. Logger is interfering in annoying ways.
     assert result.stdout.endswith("Testing PANOPTES Unit\n")
@@ -48,17 +48,17 @@ def test_cli_server(runner, config_path, cli_config_port):
 
 @pytest.mark.skip("Not working")
 def test_config_server_cli(runner, cli_server, cli_config_port):
-    result = runner.invoke(config_server_cli, ['--verbose', 'get', f'name', '--port', f'{cli_config_port}'])
+    result = runner.invoke(config_server_cli, ['--verbose', '--port', f'{cli_config_port}', 'get', f'name'])
     assert result.exit_code == 0
     # Ugh. I hate this. Logger is interfering in annoying ways.
     assert result.stdout.endswith("Testing PANOPTES Unit\n")
 
     # Set the name.
-    result = runner.invoke(config_server_cli, ['set', '--port', f'{cli_config_port}', f'name', f'foobar'])
+    result = runner.invoke(config_server_cli, ['--port', f'{cli_config_port}', 'set', f'name', f'foobar'])
     assert result.exit_code == 0
     assert result.stdout.endswith("\n{'name': 'foobar'}\n")
 
     # Get the name.
-    result = runner.invoke(config_server_cli, ['get', f'name', '--port', f'{cli_config_port}'])
+    result = runner.invoke(config_server_cli, ['--port', f'{cli_config_port}', 'get', f'name'])
     assert result.exit_code == 0
     assert result.stdout.endswith("\nfoobar\n")
