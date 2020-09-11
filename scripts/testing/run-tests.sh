f98632c7b902fa13c07f67f43348b06b658d7779
Wilfred Gee <wtylergee@gmail.com>
2020-09-11 13:40:21 -1000

    * Try something different.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -2,6 +2,8 @@
 set -e
 
 COVERAGE_REPORT_FILE=${COVERAGE_REPORT_FILE:-/var/panoptes/logs/coverage.xml}
+PANOPTES_CONFIG_HOST="${PANOPTES_CONFIG_HOST:-localhost}"
+PANOPTES_CONFIG_PORT="${PANOPTES_CONFIG_PORT:-8765}"
 
 # This assumes we are always running in a docker container.
 export COVERAGE_PROCESS_START="/var/panoptes/panoptes-utils/setup.cfg"
@@ -11,19 +13,20 @@ coverage erase
 echo "Figure out where we are: ${PWD}"
 echo "Figure out PATH: ${PATH}"
 
-echo "Checking to make sure panoptes-config-server is running"
-scripts/wait-for-it.sh --timeout=30 --strict panoptes-config-server:8765 -- echo "Found panoptes-config-server, starting tests."
-
 id
 ls -la
 
 # Run coverage over the pytest suite.
-echo "Starting config server"
-coverage run "panoptes-config-server --verbose run"
+echo "Starting config server in background"
+panoptes-config-server --verbose --host "${PANOPTES_CONFIG_HOST}" --port "${PANOPTES_CONFIG_PORT}" run --no-load-local --no-save-local &
+
+echo "Checking to make sure panoptes-config-server is running"
+scripts/wait-for-it.sh --timeout=30 --strict "${PANOPTES_CONFIG_HOST}:${PANOPTES_CONFIG_PORT}" -- echo "Config-server up"
+
 echo "Starting testing"
 coverage run "$(command -v pytest)"
 echo "Stopping config server"
-coverage run "panoptes-config-server --verbose stop"
+panoptes-config-server --verbose --host "${PANOPTES_CONFIG_HOST}" --port "${PANOPTES_CONFIG_PORT}" stop
 
 echo "Combining coverage for ${COVERAGE_REPORT_FILE}"
 coverage combine
