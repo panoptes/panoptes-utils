f395d19d81f028f20341659dcc7d58981aaac17f
Wilfred Gee <wtylergee@gmail.com>
2020-09-15 09:35:38 -1000

    * Annoying imports.

--- src/panoptes/utils/__init__.py
+++ src/panoptes/utils/__init__.py
@@ -16,7 +16,12 @@ from panoptes.utils.utils import get_quantity_value
 from panoptes.utils.utils import altaz_to_radec
 from panoptes.utils.utils import image_id_from_path
 from panoptes.utils.utils import sequence_id_from_path
-from panoptes.utils.time import current_time, flatten_time, wait_for_events, CountdownTimer
+from panoptes.utils.utils import get_free_space
+from panoptes.utils.utils import string_to_params
+from panoptes.utils.time import current_time
+from panoptes.utils.time import flatten_time
+from panoptes.utils.time import wait_for_events
+from panoptes.utils.time import CountdownTimer
 
 from loguru import logger
 
