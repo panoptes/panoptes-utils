adfa36c923b2cf47db091aeab931137c03a774dc
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-01-11 09:30:55 -1000

    * Bigger python modules use system libs.

--- tests/start-tests.sh
+++ tests/start-tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f tests/Dockerfile -t panoptes-utils:testing .
+echo "Preparing tests"
+docker build --quiet -f tests/Dockerfile -t panoptes-utils:testing .
 docker run --rm -i -v "logs:/var/panoptes/logs" --env-file tests/env --network host panoptes-utils:testing
