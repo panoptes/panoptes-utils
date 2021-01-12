f0f498ed212860c12a383456336d97be835070b4
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-01-12 12:08:12 -1000

    * More explicit start/stop of config server.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,7 +5,10 @@ PANOPTES_CONFIG_HOST="localhost"
 PANOPTES_CONFIG_PORT="8765"
 
 echo "Starting config server in background"
-panoptes-config-server --verbose run --no-load-local --no-save-local &
+panoptes-config-server --verbose run --no-load-local --no-save-local \
+  --config-host "${PANOPTES_CONFIG_HOST}" \
+  --config-port "${PANOPTES_CONFIG_PORT}" \
+  --config-file "${PANOPTES_CONFIG_FILE}" &
 
 echo "Checking to make sure panoptes-config-server is running"
 wait-for-it --timeout=30 --strict "${PANOPTES_CONFIG_HOST}:${PANOPTES_CONFIG_PORT}" -- echo "Config-server up"
@@ -14,6 +17,9 @@ echo "Starting testing"
 pytest
 
 echo "Stopping config server"
-panoptes-config-server --verbose stop
+panoptes-config-server --verbose stop \
+  --config-host "${PANOPTES_CONFIG_HOST}" \
+  --config-port "${PANOPTES_CONFIG_PORT}" \
+  --config-file "${PANOPTES_CONFIG_FILE}"
 
 exit 0
