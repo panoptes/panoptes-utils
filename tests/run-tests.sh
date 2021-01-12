755c195594c16d826accc40a852105d66bb15d89
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-01-12 13:26:00 -1000

    * Restrict the `Pillow` version because of current bug. * Fix test runners. * Update GHA testing.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-PANOPTES_CONFIG_FILE="tests/testing.yaml"
-PANOPTES_CONFIG_HOST="localhost"
-PANOPTES_CONFIG_PORT="8765"
+export PANOPTES_CONFIG_FILE="tests/testing.yaml"
+export PANOPTES_CONFIG_HOST="localhost"
+export PANOPTES_CONFIG_PORT="8765"
+export PANLOG="logs"
 
 echo "Starting config server in background"
-panoptes-config-server --verbose run --no-load-local --no-save-local \
-  --config-host "${PANOPTES_CONFIG_HOST}" \
-  --config-port "${PANOPTES_CONFIG_PORT}" \
-  --config-file "${PANOPTES_CONFIG_FILE}" &
+panoptes-config-server --verbose \
+  --host "${PANOPTES_CONFIG_HOST}" \
+  --port "${PANOPTES_CONFIG_PORT}" \
+  run \
+  --config-file "${PANOPTES_CONFIG_FILE}" \
+  --no-load-local --no-save-local &
 
 echo "Checking to make sure panoptes-config-server is running"
 wait-for-it --timeout=30 --strict "${PANOPTES_CONFIG_HOST}:${PANOPTES_CONFIG_PORT}" -- echo "Config-server up"
 
 echo "Starting testing"
-pytest
+pytest-3
 
 echo "Stopping config server"
-panoptes-config-server --verbose stop \
-  --config-host "${PANOPTES_CONFIG_HOST}" \
-  --config-port "${PANOPTES_CONFIG_PORT}" \
-  --config-file "${PANOPTES_CONFIG_FILE}"
+panoptes-config-server --verbose \
+  --host "${PANOPTES_CONFIG_HOST}" \
+  --port "${PANOPTES_CONFIG_PORT}" \
+  stop
 
 exit 0
