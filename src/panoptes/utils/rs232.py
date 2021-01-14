fb8bd5429e4b293df22a4d721f31100aaf65f218
JAAlvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2021-01-15 10:40:11 +1100

    Restructured new readlines() method

--- src/panoptes/utils/rs232.py
+++ src/panoptes/utils/rs232.py
@@ -235,25 +235,15 @@ class SerialData(object):
         return ''
 
     def readlines(self, retry_limit=None, retry_delay=None):
-        """Reads next line of input using readline.
-
-        If no response is given, delay for retry_delay and then try to read
-        again. Fail after retry_limit attempts.
+        """Reads lines of input using readlines.
         """
-        assert self.ser
-        assert self.ser.isOpen()
-
-        if retry_limit is None:
-            retry_limit = self.retry_limit
-        if retry_delay is None:
-            retry_delay = self.retry_delay
-
-        for _ in range(retry_limit):
-            data = self.ser.readlines()
-            if data:
-                return [d.decode(encoding='ascii') for d in data]
-            time.sleep(retry_delay)
-        return ''
+        lines = []
+        while True:
+            line = self.read()
+            if not line:
+                break
+            lines.append(line)
+        return lines
 
     def get_reading(self):
         """Reads and returns a line, along with the timestamp of the read.
