0139b136fab3570b33107e8c4835e8e099cf45d1
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-02-16 13:57:13 -1000

    * Add all options.

--- src/panoptes/utils/cli/image.py
+++ src/panoptes/utils/cli/image.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Union
 
 import typer
 
@@ -8,10 +9,20 @@ app = typer.Typer()
 
 
 @app.command('convert-cr2')
-def cr2_to_fits(file_path: Path, remove_cr2: bool = True) -> Path:
+def cr2_to_fits(
+        cr2_fname: Union[str, Path],
+        fits_fname: str = None,
+        overwrite: bool = False,
+        fits_headers: dict = None,
+        remove_cr2: bool = False,
+) -> Path:
     """Convert a CR2 image to a FITS, return the new path name."""
-    print(f'Converting {file_path} to FITS')
-    fits_fn = cr2.cr2_to_fits(file_path, remove_cr2=remove_cr2)
+    print(f'Converting {cr2_fname} to FITS')
+    fits_fn = cr2.cr2_to_fits(cr2_fname,
+                              fits_fname=fits_fname,
+                              overwrite=overwrite,
+                              fits_headers=fits_headers,
+                              remove_cr2=remove_cr2)
     print(f'FITS file available at {fits_fn}')
 
     return Path(fits_fn)
