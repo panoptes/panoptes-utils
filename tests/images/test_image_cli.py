7392317d2113b8699259211ca1adb08011066389
Wilfred Gee <wtylergee@gmail.com>
2022-08-05 10:33:09 -1000

    Set annoying message to trace.

--- tests/images/test_image_cli.py
+++ tests/images/test_image_cli.py
@@ -10,4 +10,4 @@ def test_solve_cli(unsolved_fits_file):
     result = runner.invoke(fits_app, [unsolved_fits_file])
     print(result)
     assert result.exit_code == 0
-    assert f'Plate-solved file available at' in result.stdout
+    assert 'Plate-solved file available at' in result.stdout
