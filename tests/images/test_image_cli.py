964a50084963b8812356529a705af5c8231c1272
Wilfred Gee <wtylergee@gmail.com>
2022-08-05 10:27:51 -1000

    * Fix runner test.

--- tests/images/test_image_cli.py
+++ tests/images/test_image_cli.py
@@ -7,7 +7,7 @@ runner = CliRunner()
 
 def test_solve_cli(unsolved_fits_file):
     print(f'Testing {unsolved_fits_file}')
-    result = runner.invoke(fits_app, f'solve {unsolved_fits_file}')
+    result = runner.invoke(fits_app, [unsolved_fits_file])
     print(result)
     assert result.exit_code == 0
     assert f'Plate-solved file available at' in result.stdout
