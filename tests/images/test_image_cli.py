9065c55ce6690898faf60c7acaf560e4421abc77
Wilfred Gee <wtylergee@gmail.com>
2022-08-05 10:11:23 -1000

    * Adding `cfitsio` for `fpack` testing. * Test fixes for coverage.

--- tests/images/test_image_cli.py
+++ tests/images/test_image_cli.py
@@ -6,7 +6,8 @@ runner = CliRunner()
 
 
 def test_solve_cli(unsolved_fits_file):
-    result = runner.invoke(fits_app, ['solve', unsolved_fits_file])
+    print(f'Testing {unsolved_fits_file}')
+    result = runner.invoke(fits_app, f'solve {unsolved_fits_file}')
     print(result)
     assert result.exit_code == 0
     assert f'Plate-solved file available at' in result.stdout
