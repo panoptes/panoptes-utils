2cde5dbd96bac0f95f6c89813f6637d77b9fb9d0
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-04-02 10:46:08 -1000

    * More coverage

--- src/panoptes/utils/utils.py
+++ src/panoptes/utils/utils.py
@@ -155,6 +155,9 @@ def get_quantity_value(quantity, unit=None):
     >>> get_quantity_value('J2000.0', unit='jyear_str')
     'J2000.0'
 
+    >>> get_quantity_value('J2000.0')
+    'J2000.0'
+
     Args:
         quantity (astropy.units.Quantity or scalar): Quantity to extract numerical value from.
         unit (astropy.units.Unit, optional): unit to convert to.
